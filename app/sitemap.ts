795476410795e93f013fdea97933ac9dd1b2728d
ltmervin123 <ltmervin123@gmail.com>
2025-12-30 20:43:02 +0800

    Fix: Update sitemap URL to reflect the correct domain for consistency

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://alvincentsangco.dev/",
+      url: "https://alvin-portfolio-website.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
